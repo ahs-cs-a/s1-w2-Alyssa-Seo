7b1e61db067670aa26c7d6281184f1d0ac533050
Alyssa-Seo <alyssajs26@ausdk12.org>
2024-08-23 17:04:50 +0000

    week 2!

--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,12 +1,15 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater("Dead Sea", "true", 15, 100);
-        BodyOfWater b1= new BodyOfWater("Dead Sea, 1000");
+        BodyOfWater b = new BodyOfWater("Dead Sea", true, 15, 100);
+        BodyOfWater b1= new BodyOfWater("Dead Sea", 1000);
+    
+        System.out.println(b.name());
+        System.out.println(b1.name());
+        System.out.println(b.largestDiameter());
+        System.out.println(b1.largestDiameter());
+        System.out.println(b1.avgDepth());
+        System.out.println(b.isSaltWater());
+        b1. setname("arctic circle");
+        System.out.println(b1.name());
     }
-        System.out.println(b.name);
-        System.out.println(b1.name);
-        System.out.println(b.largestDiameter);
-        System.out.println(b1.largestDiameter);
-        System.out.println(b1.avgDepth);
-        System.out.println(b.isSaltWater);
 }
