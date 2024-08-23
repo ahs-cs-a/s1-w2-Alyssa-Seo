e58cdea3c6497d6f6093bd0ab594598a533da333
Alyssa-Seo <alyssajs26@ausdk12.org>
2024-08-23 16:58:24 +0000

    i hate this

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,41 @@
+public class BodyOfWater {
+    private String name;
+    private boolean isSaltWater;
+    private double largestDiameter;
+    private double avgDepth;
+    
+    public BodyOfWater(String n, boolean s, double l, double D){
+        name = n;
+        isSaltWater = s;
+        largestDiameter = l;
+        avgDepth = D;
+    }
 
+    public BodyOfWater(String n, double D) {
+        name = n;
+        avgDepth = D;
+    }
+
+    public void setname(String n){
+        name = n;
+    }
+    public void setlargestDiameter(double D){
+        largestDiameter = D;
+    }
+
+    public double avgDepth(){
+        return avgDepth;
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public double largestDiameter(){
+        return largestDiameter;
+    }
+
+    public boolean isSaltWater(){
+        return isSaltWater;
+    }
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,12 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Dead Sea", "true", 15, 100);
+        BodyOfWater b1= new BodyOfWater("Dead Sea, 1000");
     }
+        System.out.println(b.name);
+        System.out.println(b1.name);
+        System.out.println(b.largestDiameter);
+        System.out.println(b1.largestDiameter);
+        System.out.println(b1.avgDepth);
+        System.out.println(b.isSaltWater);
 }
